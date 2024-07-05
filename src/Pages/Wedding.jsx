5384f3e67b2fdf5939ebe04855c39c217353260a
Karthikeyan Paramasivam <Karthikeyan210595@gmail.com>
2024-07-05 20:54:38 +0530

    updated code

--- src/Pages/Wedding.jsx
+++ src/Pages/Wedding.jsx
@@ -93,7 +93,7 @@ export default function Wedding() {
         dots: false,
         infinite: true,
         speed: 5000,
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
@@ -103,8 +103,8 @@ export default function Wedding() {
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: true,
                 dots: true,
               },
